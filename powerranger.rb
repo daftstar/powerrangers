8e28d644e2096bd0d72269e84f4079639133bc9f
Nik Daftary <nikdaftary@gmail.com>
2013-09-10 16:31:11 -0500

    inital class and methods setup

--- powerranger.rb
+++ powerranger.rb
@@ -1,7 +1,34 @@
 class Person
+	attr_accessor :name, :caffeine_level
+
+	def initialize 
+		@name = name
+		@caffeine_level = caffeine_level
+
+	def run
+	end
+
+
+	def drink_coffee
+	end
+
+
+	def scream
+	end
+
+
+
 end
 
-class PowerRanger
+
+
+
+
+
+
+
+class PowerRanger < Person
+
 end
 
 class EvilNinja
