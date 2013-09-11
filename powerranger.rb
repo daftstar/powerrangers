6ed5a7b4e6b945b961b631c12e3ef5f8701c0600
Nik Daftary <nikdaftary@gmail.com>
2013-09-10 20:03:57 -0500

    using mixins.  Code functionally works, but logically has issues

--- powerranger.rb
+++ powerranger.rb
@@ -1,13 +1,63 @@
+#-------------------- MIXIN: Fight_abilities ---------------------------
+
+module Fight_abilities
+
+	# METHOD PUNCH --------------------------
+
+	def punch(person_on_receiving_end)
+			puts "#{@name} punches " + person_on_receiving_end.to_s
+			puts ">--|-@"
+			# if person punching has strenght > 5,
+			# 	person on receiving end will run away twice
+			# else person on receiving end will run away once. 
+
+			if @strength > 5
+
+					puts "#{person_on_receiving_end.to_s} runs away twice!"
+					puts " ~~~~-->>>     >--|D;"
+					person_on_receiving_end.run
+					person_on_receiving_end.scream
+					person_on_receiving_end.run
+					puts "---------------------"
+				else
+					person_on_receiving_end.run
+					puts "---------------------"
+			end
+			person_on_receiving_end.lose_some_caffeine
+			puts "#{@name}, your caffeine has dropped to #{@caffeine_level}"
+		end
+
+	# METHOD USE_MEGAZORD --------------------------	
+	def use_megazord(person_on_receiving_end) # person_on_receiving end is the argument passed in
+
+		puts "#{@name} unleashes the megazord combo on " + person_on_receiving_end.to_s #to_s defined above
+		punch(person_on_receiving_end)
+		person_on_receiving_end.lose_so_much_life #calls method lose_so_much life from Person class
+	end		
+
+	# METHOD CAUSE_MAYHEM --------------------------
+		def cause_mayhem(person_on_receiving_end)
+			person_on_receiving_end.lose_all_caffeine
+		end
+
+end
+
+
+
+
+
+
+
 class Person
-	attr_accessor :name, :caffeine_level, :life
+
+	attr_accessor :name, :caffeine_level, :life 
 
 	def initialize(name, caffeine_level)
 		@name = name
 		@caffeine_level = caffeine_level
 		@life = 1000
-		puts "Person #{@name}. You have #{@caffeine_level} caffeine and #{@life} life points."
+		#puts "Person #{@name}. You have #{@caffeine_level} caffeine."#and #{@life} life points."
 	end
-
 
 
 	def run
@@ -39,13 +89,31 @@ class Person
 		end
 	end
 
-	def to_s
-		return "#{@name}" # puts will return a nil... must use return
+
+	def lose_some_caffeine
+		@caffeine_level -= 10
+		puts "#{@name} is sooo sleepy..zzzzz."
+		puts "caffeine is at: #{@caffeine_level}"
 	end
 
 
+	def lose_all_caffeine
+		@caffeine_level = 0
+		puts "#{@name} is sooo sleepy..zzzzz"
+		puts "#{@name}'s caffeine is now at #{@caffeine_level}"
+	end
 
 
+	def recover
+		@life +=10
+		@caffeine_level +=100
+		puts "#{@name}, life: #{@life}  caffeine: #{caffeine_level}"
+	end
+
+
+	def to_s
+		return "#{@name}" # puts will return a nil... must use return
+	end
 end
 
 
@@ -53,96 +121,93 @@ end
 #-----------------  CLASS POWER RANGER ---------------#
 
 class PowerRanger < Person
+	include Fight_abilities
+
 	attr_accessor :strength, :color
 
 	def initialize(name, caffeine_level, strength)
+		super(name,caffeine_level)
+
 		@strength = strength
 		@caffeine_level = caffeine_level
 		@strength = strength
 		@name = name
 		@life = 5000
 
-		puts "Hello power ranger #{@name}. You have #{@caffeine_level} caffeine, #{@strength} strength and #{@life} life. "
+		puts "#{@name} is born as a Power Ranger. #{@name} has #{@caffeine_level} caffeine, #{@strength} strength and #{@life} life. "
 	end
-
-
-	def punch(person_on_receiving_end)
-		puts "#{@name} punches " + person_on_receiving_end.to_s
-		puts ">--|-@"
-		# if person punching has strenght > 5,
-		# 	person on receiving end will run away twice
-		# else person on receiving end will run away once. 
-
-		if @strength > 5
-
-				puts "#{person_on_receiving_end.to_s} runs away twice!"
-				puts " ~~~~-->>>     >--|D;"
-				person_on_receiving_end.run
-				person_on_receiving_end.scream
-				person_on_receiving_end.run
-				puts "---------------------"
-			else
-				person_on_receiving_end.run
-				puts "---------------------"
-		end
-		@caffeine_level -= 5
-		puts "#{@name}, your caffeine has dropped to #{@caffeine_level}"
-	end
-
-
-	def use_megazord(person_on_receiving_end)
-
-		puts "#{@name} unleashes the megazord combo on " + person_on_receiving_end.to_s
-		punch(person_on_receiving_end)
-
-		person_on_receiving_end.lose_so_much_life
-	end
-
-
-	def rest
-		@strength += 5
-		@life +=10
-	end
-
 end
+
+
+
+
 
 
 #-----------------  CLASS EVIL NINJA ---------------#
 
 class EvilNinja < Person
+	include Fight_abilities
+
+
 	attr_accessor :strength, :evilness
 
-		def initialize(name, caffeine_level, strength)
+		def initialize(name, caffeine_level, strength)	
+		super(name, caffeine_level)
 		@strength = strength
-		@caffeine_level = caffeine_level
-		@strength = strength
-		@name = name
+		#@caffeine_level = caffeine_level (we're keeping caffeine level the same)
+		#@strength = strength
+		#@name = name
 		@life = 5000
-		@evilness = evilness
+		@evilness = rand(1000)
+
+		puts "#{@name} is born as an evil lord. #{@name} has a strength of #{@strength} and is level #{@evilness} evil."
 	end
+end
+
+
+
+
+
+
+#---------------- RUNNING TESTS --------------------------# 
+
+def fight_scene
+
+		person1 = Person.new("Bebel",55)
+		#puts""
+
+		person2 = Person.new("Jermaine",45)
+		puts""
+
+		pr1 = PowerRanger.new("Pinky", 100, 99)
+		#puts""
+		pr1.punch(person1)
+
+		#pr2 = PowerRanger.new("Bleu", 100, 3)
+		#puts""
+
+		#evil1 = EvilNinja.new("EVIL GUY", 9000, 1000)
+		#evil2 = EvilNinja.new("EVIL GAL", 10000, 1100)
+
+		#person1.drink_coffee
+		#pr1.drink_coffee
+
+
+		puts "--------"
+
+
+
+		#pr1.punch(person2)
+		#pr1.use_megazord(person2)
+		#.cause_mayhem(person1)
+		#pr1.punch(person1)
+		#person1.recover
+
+		#pr1.punch(person1)
 
 
 end
 
-
-#---------------- RUNNING TESTS --------------------------# 
-person1 = Person.new("Bebel",45)
-person2 = Person.new("Jermaine",45)
-pr1 = PowerRanger.new("Pinky", 100, 99)
-pr2 = PowerRanger.new("Bleu", 100, 3)
-
-#person1.drink_coffee
-#pr1.drink_coffee
-
-
-puts "--------"
-
-
-
-#pr1.punch(person2)
-pr1.use_megazord(person2)
-
-
-
+fight_scene
 
 
